9ba0fa3730c3942e17ee46efe1f12718d463bf9f
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-04-19 05:09:50 +0530

    Update settings.py

--- GroovyMusic/plugins/bot/settings.py
+++ GroovyMusic/plugins/bot/settings.py
@@ -94,7 +94,7 @@ async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
             InputMediaPhoto(
                 media=START_IMG_URL,
                 _["start_2"].format(CallbackQuery.from_user.mention, app.mention),
-            ),
+            )
             reply_markup=InlineKeyboardMarkup(buttons),
         )
     else:
