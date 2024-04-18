ad883cc1f1d4e63e59e6e004bf506f1866a561d0
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-04-19 00:27:32 +0530

    REDZONExMUSlC

--- GroovyMusic/plugins/misc/autoleave.py
+++ GroovyMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001626004802
-                                and i.chat.id != --1001754457302
+                                and i.chat.id != -1002133369721
+                                and i.chat.id != -1002104732521
                             ):
                                 if left == 20:
                                     continue
