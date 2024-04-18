1446bc0ce2f4defb9d415dc9e0daa2f15b9c4be1
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-04-19 04:01:25 +0530

    REDZONExMUSlC

--- GroovyMusic/plugins/bot/settings.py
+++ GroovyMusic/plugins/bot/settings.py
@@ -66,6 +66,16 @@ async def settings_cb(client, CallbackQuery, _):
         reply_markup=InlineKeyboardMarkup(buttons),
     )
 
+@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
+@languageCB
+async def gib_repo(client, CallbackQuery, _):
+    await CallbackQuery.edit_message_media(
+        InputMediaVideo("https://telegra.ph/file/93d7603ac33a8570aafd9.mp4"),
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+        ),
+    )
+
 
 @app.on_callback_query(filters.regex("settingsback_helper") & ~BANNED_USERS)
 @languageCB
